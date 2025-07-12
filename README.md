4ad5ec38f62a4d6ee74c4c2ac82baa4167d07fcb
FPF7 <fpf@umich.edu>
2025-07-12 10:03:17 +0000

    instructions to README auto library installation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TranscriptFormatter
+
+How to Use:
+
+On the terminal, type the following:
+python3 TranscriptFormatter.py
+
+A library to read the VTT files should start downloading if it is the first time using it.
+
+After it downloads, terminal will prompt you to enter one of the following:
+*The name of your VTT file, which should be placed in the same folder as the TranscriptFormatter.py file
+*The name of a folder with all your VTT files 
--- TranscriptFormatter.py
+++ TranscriptFormatter.py
@@ -1,13 +1,17 @@
-import webvtt
 import os
+try:
+    import webvtt
+except ImportError:
+    os.system("pip install webvtt-py")
+    import webvtt
 base_dir = os.path.dirname(__file__)
 formatted_folder = os.path.join(base_dir, "formatted_transcripts")
 os.makedirs(formatted_folder, exist_ok=True)
 def write_text(txt_folder,vtt_name):
     try:
         read_vtt = webvtt.read(vtt_folder_single)
-    except Exception as e:
-        print("❌ "+ vtt_name +" file not found, check spelling. Make sure to include .vtt at the end of the file. (Ex. 2432_transcript_FP.vtt) ❌")
+    except Exception as e1:
+        print("❌ "+ vtt_name +" file/folder not found, check spelling. If it is a single file, include .vtt at the end. (Ex. 2432_transcript_FP.vtt) ❌")
         exit()
     txt_name = vtt_name[:-4]+".txt"
     txt_directory = os.path.join(txt_folder, txt_name)
@@ -32,7 +36,12 @@ if vtt_file.endswith(".vtt"):
     write_text(formatted_folder,vtt_file)
 else: 
     vtt_folder = os.path.join(base_dir, vtt_file)
-    for filename in os.listdir(vtt_folder):
+    try:
+        vtt_list = os.listdir(vtt_folder)
+    except Exception as e2:
+        print("❌ "+ vtt_file +" file/folder not found, check spelling. If it is a single file, include .vtt at the end. (Ex. 2432_transcript_FP.vtt) ❌")
+        exit()
+    for filename in vtt_list:
         if filename.endswith(".vtt"):
             vtt_folder_single = os.path.join(vtt_folder, filename)
             write_text(formatted_folder,filename)
